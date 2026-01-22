29467fe37756e20fa1dcd9a8fab25e085997c093
Dirkjan Ochtman <dirkjan@ochtman.nl>
2026-01-22 16:51:53 +0100

    Display certificate chain before output

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,7 +15,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	resp, err := http.Get(os.Args[1])
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+				// Standard chain verification has already passed at this point.
+				// Add your custom validation logic here.
+
+				fmt.Fprintf(os.Stderr, "Certificate chain has %d verified chain(s)\n", len(verifiedChains))
+				for i, chain := range verifiedChains {
+					fmt.Fprintf(os.Stderr, "Chain %d:\n", i)
+					for j, cert := range chain {
+						fmt.Fprintf(os.Stderr, "  [%d] %s\n", j, cert.Subject.CommonName)
+					}
+				}
+				fmt.Fprintln(os.Stderr)
+
+				return nil
+			},
+		},
+	}
+
+	client := &http.Client{Transport: transport}
+
+	resp, err := client.Get(os.Args[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
